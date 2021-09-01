1331df16c27d6a062a411bc0bc6263be2ed2b0c6
Michal Markowski <marmichal>
2021-09-01 23:40:44 +0200

    Prevent strong capture inside client when making network call

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -23,7 +23,8 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { result in
+		client.get(from: url) { [weak self] result in
+			guard self != nil else { return }
 			switch result {
 			case .failure(_):
 				completion(.failure(RemoteFeedLoader.Error.connectivity))
