07c8899ac51d8f4a4ff67107020c2e5de60e7ca9
Michal Markowski <marmichal>
2021-09-01 10:30:23 +0200

    Handle first error. Make first test pass.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -20,6 +20,7 @@ public final class RemoteFeedLoader: FeedLoader {
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
+			completion(.failure(RemoteFeedLoader.Error.connectivity))
 		}
 	}
 }
